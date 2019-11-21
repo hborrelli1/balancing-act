8f52a06c8c16e5469ce51cabab3a67050e3298d7
Harry Borrelli <hborrelli@outlook.com>
2019-11-20 23:01:00 -0700

    Add slide animation on closing intro box

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 function closeIntro() {
-  document.getElementById("intro-box").style.display = 'none';
+  // document.getElementById("intro-box").style.display = 'none';
+  // var boxHeight = document.getElementById("intro-box").offsetHeight;
+  // document.getElementById("intro-box").style.margin = `${boxHeight} 0 0 0`;
+  document.getElementById("intro-box").classList.add('hidden');
 }
